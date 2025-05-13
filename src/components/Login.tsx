1776bce4cd59f8908754ccf8430fe0698979f9e7
Adrian <60854914+AdieDevStore@users.noreply.github.com>
2025-05-13 12:23:15 +0200

    learing continues;

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,10 @@
 import '../styles/Login.css'
+import loginUser from '../utils/login'
 
 function Login() {
   return (
     <section className='login__container' >
-        <button className='login__container__btn__grp'><img src="src/assets/google-light-small.png" /><p>Sign in with Google</p></button>
-        <button className='login__container__btn__grp'><img src="src/assets/apple-dark-small.png" /><p>Sign in with Apple</p></button>
+        <button className='login__container__btn__grp' onClick={loginUser}><img src="src/assets/spotify_logo.png" /><p>Sign in with Spotify</p></button>
     </section>
   )
 }
